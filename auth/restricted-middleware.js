e7370710ee66374e2009722c90b4cd7c2c0cc2ab
Eian Carter <eian@bricleir.com>
2020-02-27 11:28:57 -0800

    fixed restricted middleware

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,59 +1,21 @@
-const router = require("express").Router();
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const Users = require("../users/users-model");
-const { jwtSecret } = require("../config/secrets");
+const { jwtSecret } = require("../config/secrets.js");
 
-router.post("/register", (req, res) => {
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 8);
-    user.password = hash;
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
 
-    Users.add(user)
-        .then(saved => {
-            res.status(201).json(saved);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
-});
+  if (authorization) {
+    jwt.verify(authorization, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        req.decodedToken = decodedToken;
 
-router.post("/login", (req, res) => {
-    let {username, password } = req.body;
-
-    Users.findBy({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                const token = generateToken(user);
-
-                res.status(200).json({
-                    message: `Welcome ${user.username}`,
-                    token,
-                });
-            } else {
-                res.status(401).json({ message: "invalid credentials" });
-            }
-        })
-        .catch(error => {
-            console.log("ERROR: ", error);
-            res.status(500).json({ error: "/login error" });
-        });
-});
-
-function generateToken(user) {
-    const payload = {
-        subject: user.id,
-        username: user.username,
-        role: user.role || "user",
-    };
-    
-    const options = {
-        expiresIn: "1hr"
-    };
-
-    return jwt.sign(payload, jwtSecret, options);
-}
-
-module.exports = router;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+};
